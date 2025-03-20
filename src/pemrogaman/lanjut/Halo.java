27c0c86a1835018204a27dfc0c74dd21340bc23c
Faiq <mhsfaiqul24@gmail.com>
2025-03-20 23:55:57 +0700

    First commit

---
+++ src/pemrogaman/lanjut/Halo.java
@@ -0,0 +1,8 @@
+package pemrogaman.lanjut;
+
+public class Halo {
+    public static void main(String[] args) {
+        System.out.println("Halo Semarang");
+    }
+}
+
---
+++ src/pemrogaman/lanjut/HaloJakarta.java
@@ -0,0 +1,8 @@
+package pemrogaman.lanjut;
+
+public class HaloJakarta {
+    public static void main(String[] args) {
+        System.out.println("Halo Jakarta");
+    }
+}
+
